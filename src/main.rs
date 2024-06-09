4f47201f86e20db53e592f9dbda35a8c688e05b7
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-06-09 21:52:08 +0200

    Move app startup to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use tokio::net::TcpListener;
 use tokio::sync::broadcast;
 
+mod app;
 mod commands;
 mod data;
 mod encoding;
@@ -16,24 +16,5 @@ async fn main() -> Result<(), anyhow::Error> {
     let (database, redis_server) = server::RedisServer::from_args().await?;
     let address = redis_server.address().await;
 
-    let listener = TcpListener::bind(&address)
-        .await
-        .map_err(|e| anyhow::anyhow!("Failed to bind to address {}: {}", address, e))?;
-    println!("Listening on {}", address);
-
-    while let Ok((stream, _)) = listener.accept().await {
-        let database = database.clone();
-        let redis_server = redis_server.clone();
-        let sender = tx.clone();
-        tokio::spawn(async move {
-            match stream::handle_stream(stream, database, redis_server, sender).await {
-                Ok(_) => {}
-                Err(e) => {
-                    eprintln!("Error handling stream: {}", e);
-                }
-            }
-        });
-    }
-
-    Ok(())
+    app::run(&address, database, redis_server, tx).await
 }
