e6ede7cb51eb16bfd698319154c45ad6061bc418
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-06-11 23:10:06 +0200

    Progress on writing first test

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,7 +1,8 @@
-use core::panic;
-use std::net::TcpListener;
 use std::sync::Mutex;
 
+use not_redis::app;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast;
 
 use not_redis::server::{
@@ -20,12 +21,12 @@ pub struct TestApp {
     pub transmitter: broadcast::Sender<Transmission>,
 }
 
-pub fn setup() -> TestApp {
+pub async fn setup() -> TestApp {
     let (tx, _) = broadcast::channel::<Transmission>(100);
 
     let database = Database::new();
 
-    let port = get_available_port();
+    let port = get_available_port().await;
     let address = Address::new("127.0.0.1".into(), port);
 
     let role = ServerRole::Master(vec![], 0, 0);
@@ -41,18 +42,31 @@ pub fn setup() -> TestApp {
 
     *PORT.lock().unwrap() = port.checked_add(1).expect("Port overflow");
 
-    TestApp {
+    let test_app = TestApp {
         database,
         redis_server,
         address,
         transmitter: tx,
-    }
+    };
+
+    let address = test_app.address.name().clone();
+    let database = test_app.database.clone();
+    let redis_server = test_app.redis_server.clone();
+    let transmitter = test_app.transmitter.clone();
+
+    tokio::spawn(async move {
+        app::run(&address, database, redis_server, transmitter)
+            .await
+            .expect("Failed to run app");
+    });
+
+    test_app
 }
 
-fn get_available_port() -> u16 {
+async fn get_available_port() -> u16 {
     let port = *PORT.lock().unwrap();
     for i in port..u16::MAX {
-        if port_available(i) {
+        if port_available(i).await {
             return i;
         }
     }
@@ -60,6 +74,16 @@ fn get_available_port() -> u16 {
     panic!("No available port found between {} and {}", port, u16::MAX);
 }
 
-fn port_available(port: u16) -> bool {
-    TcpListener::bind(("127.0.0.1", port)).is_ok()
+async fn port_available(port: u16) -> bool {
+    TcpListener::bind(("127.0.0.1", port)).await.is_ok()
+}
+
+pub async fn send_message(address: &str, message: &str, buffer_size: usize) -> (Vec<u8>, usize) {
+    let mut socket = TcpStream::connect(address).await.unwrap();
+    socket.write_all(message.as_bytes()).await.unwrap();
+
+    let mut buffer = vec![0; buffer_size];
+    let read_len = socket.read(&mut buffer).await.unwrap();
+
+    (buffer, read_len)
 }
--- tests/ping.rs
+++ tests/ping.rs
@@ -1,7 +1,13 @@
+use common::send_message;
+
 mod common;
 
 #[tokio::test]
 async fn test_ping() {
-    let _app = common::setup();
-    todo!()
+    let test_app = common::setup().await;
+    let address = test_app.address.name();
+
+    let (resp, _) = send_message(&address, "PING\r\n", 512).await;
+    let resp = String::from_utf8(resp).unwrap();
+    assert_eq!(resp, "+PONG\r\n");
 }
