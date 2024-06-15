9b5d11947490bbe81d546f9703fc8c47a802846c
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-06-15 10:18:51 +0200

    Add slave info test

--- tests/info.rs
+++ tests/info.rs
@@ -1,4 +1,5 @@
 use common::{encode_string, send_message, TestApp};
+use not_redis::server::Replication;
 
 mod common;
 
@@ -19,6 +20,35 @@ pub async fn test_info_master() {
     let want_role = "role:master";
     let want_repl_id = format!("master_replid:{}", repl_id);
     let want_repl_offset = format!("master_repl_offset:{}", repl_offset);
+
+    assert!(resp.contains(want_role));
+    assert!(resp.contains(&want_repl_id));
+    assert!(resp.contains(&want_repl_offset));
+}
+
+#[tokio::test]
+pub async fn test_info_slave() {
+    let test_app_master = TestApp::master().await;
+    let (repl_id, repl_offset) = {
+        let repl = &test_app_master.redis_server.read().await.replication;
+        let repl_id = repl.id.clone();
+        let repl_offset = repl.offset.clone();
+        (repl_id, repl_offset)
+    };
+
+    let offset = repl_offset + 100;
+    let replication = Replication {
+        id: repl_id.clone(),
+        offset: offset.clone(),
+    };
+    let test_app_slave = TestApp::slave(replication).await;
+
+    let message = encode_string("info replication");
+    let resp = send_message(&test_app_slave.address.name(), &message, 512).await;
+
+    let want_role = "role:slave";
+    let want_repl_id = format!("master_replid:{}", repl_id);
+    let want_repl_offset = format!("master_repl_offset:{}", offset);
 
     assert!(resp.contains(want_role));
     assert!(resp.contains(&want_repl_id));
