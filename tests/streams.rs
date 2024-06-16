63b3cdcb6cb63f04f57d36e06321d0fae799633e
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-06-16 13:43:21 +0200

    Complete first test in streams

--- tests/streams.rs
+++ tests/streams.rs
@@ -1,18 +1,43 @@
 use common::{encode_string, send_message, TestApp};
-use not_redis::encoding::{bulk_string, simple_string};
+use not_redis::encoding::{bulk_string, error_string, simple_string};
+use tokio::sync::broadcast::error;
 
 mod common;
 
 #[tokio::test]
-async fn specify_stream_id() {
+async fn specified_stream_id_must_be_greater_than_previous() {
     let test_app = TestApp::master().await;
     let address = test_app.address.name();
 
-    let message = encode_string("xadd cool 100-100 foo bar");
+    let message = encode_string("xadd cool 100-100 one two");
     let resp = send_message(&address, &message).await;
-    assert_eq!(resp, bulk_string("0-1"));
+    assert_eq!(resp, bulk_string("100-100"));
 
-    // TODO: Read from stream
+    let message = encode_string("xadd cool 101-99 three four");
+    let resp = send_message(&address, &message).await;
+    assert_eq!(resp, bulk_string("101-99"));
+
+    let message = encode_string("xadd cool 101-100 five six");
+    let resp = send_message(&address, &message).await;
+    assert_eq!(resp, bulk_string("101-100"));
+
+    let message = encode_string("xadd cool 101-50 seven eight");
+    let resp = send_message(&address, &message).await;
+    assert_eq!(
+        resp,
+        error_string(
+            "ERR The ID specified in XADD is equal or smaller than the target stream top item"
+        )
+    );
+
+    let message = encode_string("xadd cool 50-50 nine ten");
+    let resp = send_message(&address, &message).await;
+    assert_eq!(
+        resp,
+        error_string(
+            "ERR The ID specified in XADD is equal or smaller than the target stream top item"
+        )
+    );
 }
 
 #[tokio::test]
@@ -20,6 +45,9 @@ async fn unspecified_stream_id_must_be_greater_than_previous() {
     let test_app = TestApp::master().await;
     let address = test_app.address.name();
     assert!(true);
+
+    // let message = encode_string("xadd cool *-100 foo bar");
+    // let resp = send_message(&address, &message).await;
 }
 
 #[tokio::test]
