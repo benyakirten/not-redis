1f6f6a7e3cee63cf45fc97c8694f01304f39d66f
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-06-21 18:12:58 +0200

    Make errors in parsing be sent in stream [ci skip]

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,7 @@
 pub fn wrong_type() -> anyhow::Error {
     anyhow::anyhow!("WRONGTYPE Operation against a key holding the wrong kind of value")
 }
+
+pub fn not_an_integer() -> anyhow::Error {
+    anyhow::anyhow!("ERR value is not an integer or out of range")
+}
